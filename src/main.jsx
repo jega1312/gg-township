e0bdc0bfc689eabb4ce44d6edcc199477154de4a
Jegathiswaran Thiaghu <thiagu1312@gmail.com>
2025-12-02 13:01:00 +0800

    Revert Flag Link Change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '/src/assets/css/index.css';
 import '/src/assets/css/international-telephone-input.css';
-import '/flags';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
